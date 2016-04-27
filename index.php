3cef86128daac4ad20d8d2b8f63b8d70ec167aae
최태근 <choetaegeun@choetaegeun-ui-MacBook-Pro.local>
2016-04-27 22:38:13 +0900

    index-basic

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/victorer.css">
+	<link rel="stylesheet" href="./css/skeleton.css">
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	
+</head>
+
+<body>
+  <header>
+
+    <div class="logo">
+      <a href="#">
+        <img id="logo-img" src="./img/logo.png">
+      </a>
+    </div>
+
+    <div class="header-item">
+      <p>LOGIN</p>
+    </div>
+
+    <div class="header-item">
+      <p>GAMBLE</p>
+    </div>
+    
+
+  </header>
+
+  <section>
+  </section>
+
+  <footer>	
+  	<div class="three columns">
+      <h5>
+        이용방법
+      </h5>
+      <p>결제</p>
+      <p>홀짝 배팅</p>
+      <p>블랙잭</p>
+    </div>
+    <div class="three columns">
+      <h5>
+        고객센터
+      </h5>
+      <p>자주 묻는 질문</p>
+      <p>이메일 문의</p>
+      <p>카카오톡 문의</p>
+    </div>
+    <div class="three columns">
+      <h5>
+        SNS
+      </h5>
+      <p>카카오톡</p>
+      <p>Instagram</p>
+      <p>Facebook</p>
+    </div>
+  </footer>
+</body>
+
+</html>
